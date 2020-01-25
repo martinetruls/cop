c1f1e6b5ad00b5f5fa5736b9aec3cb8f5d2aaffd
martinetruls <martinetrulsrud@gmail.com>
2020-01-25 17:59:42 +0100

    Renderig compulsories

---
+++ src/components/CompulsoryOverview.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    hei!
+    <li v-for="trick in items" v-bind:key="trick">{{ trick.name }}</li>
+  </div>
+</template>
+
+<script>
+import Data from "../data/compulsories.json";
+
+export default {
+  name: "CompulsoryOverview",
+  data: function() {
+    return {
+      items: Data.compulsories
+    };
+  }
+};
+</script>
