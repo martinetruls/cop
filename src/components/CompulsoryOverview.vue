34d9f5a42f09c67ffb96c17812717a658dc9f11c
martinetruls <martinetrulsrud@gmail.com>
2020-01-26 21:49:47 +0100

    Filter by type "strength"

--- src/components/CompulsoryOverview.vue
+++ src/components/CompulsoryOverview.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="overview">
-    <p>Showing: {{ compulsories.length }}</p>
+    <div class="flex">
+      <button
+        class="temp-button"
+        type="button"
+        v-on:click="compulsories = filterByStrength()"
+      >Show only strength</button>
+      <p>Showing: {{ compulsories.length }}</p>
+    </div>
+
     <div class="grid">
       <CompulsoryCard
         v-for="compulsory in compulsories"
@@ -15,6 +23,13 @@
 import Data from "../data/compulsories.json";
 import CompulsoryCard from "./CompulsoryCard";
 
+const filterByStrength = () => {
+  const filteredList = Data.compulsories.filter(
+    compulsory => compulsory.type === "Strength"
+  );
+  return filteredList;
+};
+
 export default {
   name: "CompulsoryOverview",
   components: { CompulsoryCard },
@@ -22,12 +37,31 @@ export default {
     return {
       compulsories: Data.compulsories
     };
+  },
+  methods: {
+    filterByStrength: filterByStrength
   }
 };
 </script>
 
 <style lang="scss" scoped>
 @import "../styles/vars.scss";
+
+.temp-button {
+  background-color: #f56f86; // to do
+  border: none;
+  padding: $ws-s $ws-m;
+  font-weight: 500;
+  letter-spacing: 0.5px;
+  color: white;
+  font-size: 1.4rem;
+  border-radius: 3px;
+}
+
+.flex {
+  display: flex;
+  justify-content: space-between;
+}
 
 .overview {
   padding: $ws-xxl;
