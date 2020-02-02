4b3f27dd263a2397db45978ef6a6dba0beca35a6
martinetruls <martinetrulsrud@gmail.com>
2020-02-02 18:32:39 +0100

    clean up

--- src/components/FilterToggle.vue
+++ src/components/FilterToggle.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <label :for="filterValue">{{ label }}</label>
     <input
       type="checkbox"
+      class="visually-hidden"
       :id="filterValue"
       :value="filterValue"
       :checked="isChecked"
       v-on:input="clickedFilter($event.target)"
     />
+    <label class="toggle" :for="filterValue">{{ label }}</label>
   </div>
 </template>
 
@@ -36,6 +37,7 @@ export default {
 @import "../styles/vars.scss";
 
 .toggle {
+  display: block;
   padding: 0.6rem 1rem;
   font-weight: 500;
   font-size: 1.4rem;
@@ -61,24 +63,34 @@ export default {
   }
 }
 
-.toggle-Flexibility {
-  background-color: #d0e8ec;
-  color: #008997;
+input:checked + label.toggle {
+  background-color: rgba(33, 22, 179, 1);
+  border-color: rgba(33, 22, 179, 1);
+  color: white;
 }
 
-.toggle-Strength {
-  background-color: #fbded5;
-  color: #ea4a1a;
-  border: 1px solid #ea4a1a;
+input:focus + label.toggle {
+  box-shadow: 0 0 0 4px rgba(33, 22, 179, 0.2);
 }
 
-.tag-Spins-on-static {
-  background-color: #fedee6;
-  color: #e91e64;
-}
+// .toggle-Flexibility {
+//   background-color: #d0e8ec;
+//   color: #008997;
+// }
 
-.tag-Spins-on-spinning {
-  background-color: #d6d7f2;
-  color: #2117b3;
-}
+// .toggle-Strength {
+//   background-color: #fbded5;
+//   color: #ea4a1a;
+//   border: 1px solid #ea4a1a;
+// }
+
+// .tag-Spins-on-static {
+//   background-color: #fedee6;
+//   color: #e91e64;
+// }
+
+// .tag-Spins-on-spinning {
+//   background-color: #d6d7f2;
+//   color: #2117b3;
+// }
 </style>
