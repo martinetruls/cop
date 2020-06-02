495ea9302ba9013e5bcfa2ad9a7efbc82c867601
martinetruls <martinetrulsrud@gmail.com>
2020-06-02 23:09:17 +0200

    Handling empty state when developing

--- src/components/CriteriaList.vue
+++ src/components/CriteriaList.vue
@@ -1,45 +1,60 @@
 <template>
-  <div class="criteria-list">
-    <div>
-      <section v-if="criteria.grip">
-        <h3>Grip:</h3>
-        <p>{{criteria.grip}}</p>
-      </section>
+  <div>
+    <div v-if="criteria.pointsOfContact.length" class="criteria-list">
+      <div>
+        <section v-if="criteria.grip">
+          <h3>Grip:</h3>
+          <p>{{criteria.grip}}</p>
+        </section>
 
-      <section>
-        <h3>{{`Arm${!criteria.grip && '/grip'} position:`}}</h3>
-        <p>{{criteria.armPosition}}</p>
-      </section>
+        <section v-if="criteria.armPosition">
+          <h3>{{`Arm${!criteria.grip ? '/grip' : ''} position:`}}</h3>
+          <p>{{criteria.armPosition}}</p>
+        </section>
 
-      <section>
-        <h3>Leg position:</h3>
-        <p>{{criteria.legPosition}}</p>
-      </section>
+        <section>
+          <h3>Leg position:</h3>
+          <p>{{criteria.legPosition}}</p>
+        </section>
 
-      <section>
-        <h3>Body position:</h3>
-        <p>{{criteria.bodyPosition}}</p>
-      </section>
+        <section>
+          <h3>Body position:</h3>
+          <p>{{criteria.bodyPosition}}</p>
+        </section>
+      </div>
+
+      <div>
+        <section>
+          <h3>Hold the position:</h3>
+          <p>{{criteria.holdPosition}}</p>
+        </section>
+
+        <section v-if="criteria.spinDirection">
+          <h3>Spin direction:</h3>
+          <p>{{criteria.spinDirection}}</p>
+        </section>
+
+        <section v-if="criteria.splitAngle">
+          <h3>Split angle:</h3>
+          <p>{{criteria.splitAngle}}</p>
+        </section>
+
+        <section>
+          <h3>Points of contact:</h3>
+          <ul>
+            <li
+              v-for="(contactPoint, key) in criteria.pointsOfContact"
+              :key="key"
+            >{{ contactPoint }}</li>
+          </ul>
+        </section>
+      </div>
     </div>
-
-    <div>
-      <section>
-        <h3>Hold the position:</h3>
-        <p>{{criteria.holdPosition}}</p>
-      </section>
-
-      <section v-if="criteria.splitAngle">
-        <h3>Split angle:</h3>
-        <p>{{criteria.splitAngle}}</p>
-      </section>
-
-      <section>
-        <h3>Points of contact:</h3>
-        <ul>
-          <li v-for="(contactPoint, key) in criteria.pointsOfContact" :key="key">{{ contactPoint }}</li>
-        </ul>
-      </section>
-    </div>
+    <!-- Empty state - until all data is added  -->
+    <div
+      v-if="!criteria.pointsOfContact.length"
+      class="missing-criteria"
+    >Criterias for this compulsory will be added soon</div>
   </div>
 </template>
 
@@ -88,5 +103,14 @@ h3 {
   font-weight: 700;
   font-size: 1.6rem;
   margin-bottom: 4px;
+}
+
+.missing-criteria {
+  padding: $ws-xxxl 0;
+  text-align: center;
+  margin: auto;
+  color: $dark-70;
+  font-weight: 300;
+  max-width: 30rem;
 }
 </style>
